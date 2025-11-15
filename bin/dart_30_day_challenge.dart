648c6841191a88e18472e95ce3c8b32387a95476
Sagar Dhadke <peaceunique2003@gmail.com>
2025-11-15 20:36:49 +0530

    Init commit

---
+++ bin/dart_30_day_challenge.dart
@@ -0,0 +1,5 @@
+import 'package:dart_30_day_challenge/dart_30_day_challenge.dart' as dart_30_day_challenge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_30_day_challenge.calculate()}!');
+}
---
+++ test/dart_30_day_challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_30_day_challenge/dart_30_day_challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
